8e36f19930ae20e0fbb8c5cd5cdcb858233846bf
adam <atomicpenguines@gmail.com>
2018-08-29 08:09:46 +0100

    reading json directly and with a callback function

--- file-handling/demo.js
+++ file-handling/demo.js
@@ -1,6 +1,13 @@
 var fs = require('fs')
 
+// require can be used to read json data as an object
+var data = require('./data.json')
+console.log(data.name)
+
+// this returns just a string by default
 fs.readFile('./data.json', 'utf-8', (err, data) => {
-    console.log(data)
+    // make it JSON
+    var data = JSON.parse(data)
+    console.log(data.name)
 })
 
