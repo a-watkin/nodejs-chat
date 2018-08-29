b798a32cedcaf0eea4b380dee51483fe453b96b3
adam <awatkin.work@gmail.com>
2018-08-29 14:32:54 +0100

    passed messages from postman to frontend which received them with a jquery get request to /messages

--- server-static/server.js
+++ server-static/server.js
@@ -1,8 +1,34 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express()
 
-
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+
+var messages = [
+    {
+        name: 'Bart',
+        message: 'eat my shorts'
+    },
+
+    {
+        name: 'Homer',
+        message: 'doh'
+    }
+]
+
+// url, request, response
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    // res.send(messages)
+    console.log(req.body)
+    // recieves message from postman, pushes message onto messages array, which jquery gets on the front end
+    messages.push(req.body)
+    res.sendStatus(200)
+})
 
 var server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
