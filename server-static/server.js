a735d17fda47dba7d95906a11b9a2ccc6eb20bbd
adam <atomicpenguines@gmail.com>
2018-08-30 06:50:07 +0100

    finished troubleshooting, everything working but strange bug with a bug causing app crashes on every other save - i think this is more a result of the port not being released quickly enough

--- server-static/server.js
+++ server-static/server.js
@@ -3,7 +3,23 @@ var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+// app.use(bodyParser.json())
+// var jsonParser = bodyParser.json()
+// var urlencodedParser = bodyParser.urlencoded({ extended: true })
+// app.use(bodyParser.json({ type: 'application/*+json' }))
+
 app.use(bodyParser.json())
+// without this line the app crashes
+// above works without crashing but body is undifined coming from the browser only
+
+// so it works now so above seems to be where the problem was
+// i guess urlencoded is needed
+app.use(bodyParser.urlencoded({extended: false}))
+
+// code is wroking now, but the app crashes every other save
+// i think this is due to nodemon and the port not being released fast enough
+// because it is literally every other save that causes a crash
+
 
 var messages = [
     {
@@ -24,7 +40,9 @@ app.get('/messages', (req, res) => {
 
 app.post('/messages', (req, res) => {
     // res.send(messages)
-    console.log(req.body)
+    // req.body is empty here from the front end but ok from postman
+    console.log('req.body', req.body.name)
+
     // recieves message from postman, pushes message onto messages array, which jquery gets on the front end
     messages.push(req.body)
     res.sendStatus(200)
